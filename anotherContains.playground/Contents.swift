72485fa19e324f19fa5130cf30041de3d828884f
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-17 20:30:15 -0500

    Figuring out Logic Still

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,8 +1,35 @@
 import Foundation
 
+//extension String {
+//    func anotherContains(_ text: String) -> Bool{
+//        var answer: Bool = true
+//        for letter in text{
+//            var bool: Bool? {
+//                switch self.lowercased() {
+//                case text.description = "where", "is", "WaLdO", "is wA":
+//                   answer = true
+//                    return answer
+//                case text.description = "nOPe":
+//                    answer = false
+//                    return answer
+//                default:
+//                    return nil
+//                }
+//            }
+//        }
+//        return answer
+//    }
+//}
 extension String {
-    
+    func anotherContains(_ element: String) -> Bool {
+        if element.lowercased() == self.description.lowercased(){
+            return true
+        }else{
+            return false
+        }
+    }
 }
+
 
 
 // test cases
@@ -10,3 +37,4 @@ print("Where is WaLdO".anotherContains("WALDO")) // true
 print("Where is WaLdO".anotherContains("where")) // true
 print("Where is WaLdO".anotherContains("is wA")) // true
 print("Where is WaLdO".anotherContains("nOPe"))  // false
+
