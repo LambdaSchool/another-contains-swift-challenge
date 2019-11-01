a6e125e668bb6336844d5354c39b9487e1401239
dillonp23 <dillonp.lambda@gmail.com>
2019-10-31 18:21:29 -0700

    working code submission

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,8 +1,24 @@
 import Foundation
 
 extension String {
-    func anotherContains(_ string: String) {
+    
+    func anotherContains(_ string: String) -> Bool {
         
+        var result = false
+        var count = 0
+        
+        for letter in self.lowercased() {
+            for character in string.lowercased() {
+                if letter == character {
+                    count += 1
+                    if count == string.count {
+                        result = true
+                    }
+                }
+            }
+        }
+        
+        return result
     }
 }
 
