4990fbd4c7f26d62cc544f72c8605d38e91ed255
Brad <braddiroff@icloud.com>
2020-03-11 11:30:47 -0400

    Finished coding challenge

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,7 +1,22 @@
 import Foundation
 
 extension String {
-    
+    func anotherContains(_ myString: String) -> Bool {
+        
+        var doesItMatch = false
+
+        let oneWord = components(separatedBy: " ").joined()
+        let oneAnswer = myString.components(separatedBy: " ").joined()
+        
+        print(oneWord)
+        print(oneAnswer)
+        
+        if oneWord.lowercased().range(of: oneAnswer.lowercased()) != nil {
+            doesItMatch = true
+        }
+        
+        return doesItMatch
+    }
 }
 
 
