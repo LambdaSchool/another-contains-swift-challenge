2d38d050f7a2e22b1d096c98e86058d1504b5fae
Mark Gerrior <github@gerrior.com>
2020-03-11 08:35:58 -0700

    Mark's solution

--- gerrior-contains.playground/Contents.swift
+++ gerrior-contains.playground/Contents.swift
@@ -2,14 +2,22 @@ import UIKit
 
 extension String {
     func anotherContains(_ i: String) -> Bool {
-        return self.contains(i)
+        let lhs = self.lowercased()
+        let rhs = i.lowercased()
+
+        let rangeResult = lhs.range(of: rhs)
+        
+        guard let _ = rangeResult else { return false }
+        
+        return true
     }
 }
 
 // Original is case sensitive.
-print("Where is WaLdO".contains("WALDO")) // false
-print("Where is WaLdO".contains("WaLdO")) // true
+//print("Where is WaLdO".contains("WALDO")) // false
+//print("Where is WaLdO".contains("WaLdO")) // true
 
 // My version
 print("Where is WaLdO".anotherContains("WaLdO")) // true
+print("Where is WaLdO".anotherContains("WaLdOo")) // false
 
