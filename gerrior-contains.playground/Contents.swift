0240ec23f8817130fad6ba6518b1d1d3c651e021
Mark Gerrior <github@gerrior.com>
2020-03-11 08:38:18 -0700

    Comments

--- gerrior-contains.playground/Contents.swift
+++ gerrior-contains.playground/Contents.swift
@@ -1,12 +1,13 @@
 import UIKit
 
 extension String {
-    func anotherContains(_ i: String) -> Bool {
+    func anotherContains(_ i: String) -> Bool { // TODO: Why can't I use Character here?
         let lhs = self.lowercased()
         let rhs = i.lowercased()
 
         let rangeResult = lhs.range(of: rhs)
         
+        // TODO: Ranges are weird. Help text didn't line up with this IMHO
         guard let _ = rangeResult else { return false }
         
         return true
@@ -17,7 +18,7 @@ extension String {
 //print("Where is WaLdO".contains("WALDO")) // false
 //print("Where is WaLdO".contains("WaLdO")) // true
 
-// My version
+// My version is not
 print("Where is WaLdO".anotherContains("WaLdO")) // true
 print("Where is WaLdO".anotherContains("WaLdOo")) // false
 
