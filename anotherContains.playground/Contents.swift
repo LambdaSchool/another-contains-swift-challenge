935ded6c2e7e8520ee56531df3832111c2af28bb
Libby Thomas <thomaslibby05@gmail.com>
2020-05-04 21:09:38 -0500

    Code Challenge Complete

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -2,11 +2,25 @@ import Foundation
 
 extension String {
     
+    func anotherContains(string: String) -> Bool {
+        if self.localizedCaseInsensitiveContains(string) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
 }
 
 
 // test cases
-print("Where is WaLdO".anotherContains("WALDO")) // true
-print("Where is WaLdO".anotherContains("where")) // true
-print("Where is WaLdO".anotherContains("is wA")) // true
-print("Where is WaLdO".anotherContains("nOPe"))  // false
+print("Where is WaLdO".anotherContains(string:"WALDO")) // true
+print("Where is WaLdO".anotherContains(string:"where")) // true
+print("Where is WaLdO".anotherContains(string:"is wA")) // true
+print("Where is WaLdO".anotherContains(string:"nOPe"))  // false
+print("Does thIs work".anotherContains(string: "s wo"))
+print("My Dog's name is SiRiUs".anotherContains(string: "dog"))
+print("My Dog's name is SiRiUs".anotherContains(string: "sirius"))
+print("My Dog's name is SiRiUs".anotherContains(string: "Sirius"))
+print("My Dog's name is SiRiUs".anotherContains(string: "names"))
+print("My Dog's name is SiRiUs".anotherContains(string: "Luna"))
