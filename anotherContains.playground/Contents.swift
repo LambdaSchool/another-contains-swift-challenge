edb0d9e2b593cf2c51edc9a1f0f2ca4e609a761b
jarrencampos <57583547+jarrencampos@users.noreply.github.com>
2020-05-04 20:39:46 -0700

    Finished Code Challenge

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,7 +1,16 @@
 import Foundation
 
 extension String {
-    
+    func anotherContains(_ searchString: String) -> Bool {
+        let searchString = searchString.lowercased()
+        let string = self.lowercased()
+        let check = string.range(of: searchString)
+        if check == nil {
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
 
@@ -10,3 +19,4 @@ print("Where is WaLdO".anotherContains("WALDO")) // true
 print("Where is WaLdO".anotherContains("where")) // true
 print("Where is WaLdO".anotherContains("is wA")) // true
 print("Where is WaLdO".anotherContains("nOPe"))  // false
+print("Hello".anotherContains("lo"))
